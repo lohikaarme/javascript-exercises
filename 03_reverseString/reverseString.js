37530ac02134a1eb4956dd8a6a8cc72859c260bb
lohikaarme <stewart.watt@gmail.com>
2022-04-14 23:35:12 -0700

    completed js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    const words = [...phrase];
+    let revWords = '';
 
+    for (i = 0; i < words.length;) {
+        revWords += words.pop();
+    }
+
+    return revWords
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
