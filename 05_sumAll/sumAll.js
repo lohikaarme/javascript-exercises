ed5146d5aacab0f2482e814d433f37260039625b
lohikaarme <stewart.watt@gmail.com>
2022-04-15 16:35:54 -0700

    completed exercise 5, utilized some() for checking typeof parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let sum = 0,
+        min = Math.min(...args),
+        max = Math.max(...args);
 
+    if (min < 0) return 'ERROR';
+
+    // refactored, for some arg of args checks if type is not number
+    if (args.some(arg => typeof arg != 'number')) return 'ERROR';
+
+    //old function
+    //for (let value of args) {
+    //    if (typeof(value) != 'number') return 'ERROR'
+    //};
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
+sumAll(10, '10');
 // Do not edit below this line
 module.exports = sumAll;
