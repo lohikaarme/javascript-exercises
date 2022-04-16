e898d9d34f192f4e8c1f1db885b491fdb7a7f444
lohikaarme <stewart.watt@gmail.com>
2022-04-16 16:42:24 -0700

    refactored exercise 6, added top solution to end

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,7 @@
 const leapYears = function(year) {
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        return true
-    } else {
-        return false
-    }
+    return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
 };
+
+// TOP Solution //   return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
 // Do not edit below this line
 module.exports = leapYears;
