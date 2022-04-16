da5116ecc03d30b23a42b6c0c976179bdd6546e8
lohikaarme <stewart.watt@gmail.com>
2022-04-16 16:44:41 -0700

    added top solution to end to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
 const ftoc = function(degree) {;
     return +((degree - 32) * (5 / 9)).toFixed(1)
+        // TOP Solution // return Math.round((f - 32) * (5/9) * 10) / 10;
 };
 
 const ctof = function(degree) {
     return +(degree * (9 / 5) + 32).toFixed(1)
+
+    // TOP Solution // return Math.round(((c * 9/5) + 32) * 10) / 10; 
 };
 
 // Do not edit below this line
