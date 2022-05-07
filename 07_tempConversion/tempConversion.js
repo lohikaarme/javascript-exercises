c748d28f7ed16590e90149735ce06fa89ac38465
lohikaarme <stewart.watt@gmail.com>
2022-05-06 23:55:48 -0700

    retried and refactored exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,7 @@
-const ftoc = function(degree) {;
-    return +((degree - 32) * (5 / 9)).toFixed(1)
-        // TOP Solution // return Math.round((f - 32) * (5/9) * 10) / 10;
-};
+let ctof = (deg) => parseFloat((deg * 9/5 + 32).toFixed(1));
+let ftoc = (deg) => parseFloat(((deg - 32) * 5/9).toFixed(1));
 
-const ctof = function(degree) {
-    return +(degree * (9 / 5) + 32).toFixed(1)
-        // TOP Solution // return Math.round(((c * 9/5) + 32) * 10) / 10; 
-};
 
-// Do not edit below this line
 module.exports = {
     ftoc,
     ctof
