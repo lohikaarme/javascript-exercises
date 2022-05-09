48e740cfa08b2138220817cc612d1e499735006a
lohikaarme <stewart.watt@gmail.com>
2022-05-08 23:56:31 -0700

    exercise 9, completed stirct fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    let total = 1;
+    let prevTotal = 1;
+    for (let i = 2; i < fib; ++i) {
+        total += prevTotal;
+        prevTotal = total - prevTotal;
+    };
+    return total
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci(2));
